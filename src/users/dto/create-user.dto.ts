1490b7934b4692409f10ce60de898eb30256761a
ksamrat224 <ksamrat224@gmail.com>
2025-03-22 19:47:14 +0545

    user-dto updated

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,1 +1,19 @@
-export class CreateUserDto {}
+import { IsNotEmpty, IsString } from 'class-validator';
+
+export class CreateUserDto {
+  @IsNotEmpty()
+  @IsString()
+  name: string;
+
+  @IsNotEmpty()
+  @IsString()
+  email: string;
+
+  @IsNotEmpty()
+  @IsString()
+  mobile: string;
+
+  @IsNotEmpty()
+  @IsString()
+  password: string;
+}
