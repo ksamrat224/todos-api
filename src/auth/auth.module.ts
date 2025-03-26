6836d85a36b72f049f4ac657b84f745b0d570a2a
ksamrat224 <ksamrat224@gmail.com>
2025-03-26 14:36:28 +0545

    auth controller,module updated

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
+import { PrismaClient } from '@prisma/client';
+import { UsersService } from 'src/users/users.service';
 
 @Module({
   controllers: [AuthController],
-  providers: [AuthService],
+  providers: [AuthService, PrismaClient, UsersService],
 })
 export class AuthModule {}
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,16 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaClient } from '@prisma/client';
+import { RegisterDto } from './dto/register.dto';
+import { UsersService } from 'src/users/users.service';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  constructor(
+    private readonly prisma: PrismaClient,
+    private readonly usersService: UsersService,
+  ) {}
+  async register(registerDto: RegisterDto) {
+    const user = await this.usersService.create(registerDto);
+    return user;
+  }
+}
