6d2e4c046cc872cf740a176f534fcc04d85611cf
ksamrat224 <ksamrat224@gmail.com>
2025-04-09 14:32:20 +0545

    mailer module added in app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,24 @@ import { AppService } from './app.service';
 import { TodosModule } from './todos/todos.module';
 import { UsersModule } from './users/users.module';
 import { AuthModule } from './auth/auth.module';
+import { MailerModule } from '@nestjs-modules/mailer';
 
 @Module({
-  imports: [TodosModule, UsersModule, AuthModule],
+  imports: [
+    TodosModule,
+    UsersModule,
+    AuthModule,
+    MailerModule.forRoot({
+      transport: {
+        host: process.env.SMTP_HOST,
+        port: process.env.SMTP_PORT,
+        auth: {
+          user: process.env.SMTP_USER,
+          pass: process.env.SMTP_PASSWORD,
+        },
+      },
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
