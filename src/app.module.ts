3934f82d449ff2f8cae6f586b8359d7347588be9
ksamrat224 <ksamrat224@gmail.com>
2025-04-10 14:44:47 +0545

    app module updated

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,7 +5,9 @@ import { TodosModule } from './todos/todos.module';
 import { UsersModule } from './users/users.module';
 import { AuthModule } from './auth/auth.module';
 import { MailerModule } from '@nestjs-modules/mailer';
+import { HandlebarsAdapter } from '@nestjs-modules/mailer/dist/adapters/handlebars.adapter';
 import { BullModule } from '@nestjs/bull';
+import { join } from 'path';
 
 @Module({
   imports: [
@@ -20,6 +22,10 @@ import { BullModule } from '@nestjs/bull';
           user: process.env.SMTP_USER,
           pass: process.env.SMTP_PASSWORD,
         },
+      },
+      template: {
+        dir: join(__dirname, 'templates'),
+        adapter: new HandlebarsAdapter(),
       },
     }),
     BullModule.forRoot({
